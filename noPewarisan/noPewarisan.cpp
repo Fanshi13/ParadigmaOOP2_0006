fb506d54168fc7191fd4dff00688f557a03b52d7
Fanshi13 <aquillafazanaufan@gmail.com>
2025-06-02 10:59:35 +0700

    class deriverdClass dan int main sama dari noOverriding

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -9,3 +9,21 @@ public:
         cout << "Halo saya Function dari base class";
     }
 };
+
+class derivedClass : baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Halo saya Function dari derived Class";
+    }
+};
+
+int main()
+{
+    derivedClass a;
+
+    a.perkenalan();
+
+    return 0;
+}
